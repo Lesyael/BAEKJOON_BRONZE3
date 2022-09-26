c8101ddd678e8e609c23d125f939ace0352b6cdd
Lesyael <112386686+Lesyael@users.noreply.github.com>
2022-09-26 13:56:46 +0900

    220926

---
+++ baekjoon2444.java
@@ -0,0 +1,39 @@
+package bronze3;
+
+import java.util.Scanner;
+
+public class baekjoon2444 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		
+		sc.close();
+		
+		for (int i = 1; i <= count ; i++) {
+			for (int j = 0; j < count - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 0; j < i * 2 - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+		
+		for (int i = count - 1; i >= 0 ; i--) {
+			for(int j = 0; j < count - i; j++) {
+				System.out.print(" ");
+			}
+				
+			for(int j = 0; j < i*2-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
